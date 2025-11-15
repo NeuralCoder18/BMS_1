0066521cc2dfd717c3f05273acfc23cd98ed4778
NeuralCoder18 <neuralcipher1918@gmail.com>
2025-11-15 13:41:52 +0530

    fixing color issues and possible bugs

--- bank.py
+++ bank.py
@@ -15,6 +15,14 @@ class bank():
         scrn_width=self.root.winfo_screenwidth()
         scrn_height=self.root.winfo_screenheight()
 
+        self.root.configure(bg="white")
+
+        # Force all Entry widgets to be readable
+        self.root.option_add("*Entry.background", "white")
+        self.root.option_add("*Entry.foreground", "black")
+        self.root.option_add("*Entry.insertBackground", "black")   # cursor color
+        self.root.option_add("*Entry.font", "Times 15")
+
         self.mainFrame = None         
         self.openAcFrame = None       
         self.submitFrame = None  
@@ -48,9 +56,9 @@ class bank():
             self.openAcFrame.destroy()
         
 
-        mainFrame=tk.Frame(self.root,bg="light gray",bd=5,relief="ridge")
-        mainFrame.place(x=500,y=90,width=450,height=550)
-        opLabel=tk.Label(mainFrame,text="To Open Account Click Below",font=("Times Roman",20,"bold"))
+        self.mainFrame=tk.Frame(self.root,bg="light gray",bd=5,relief="ridge")
+        self.mainFrame.place(x=500,y=90,width=450,height=550)
+        opLabel=tk.Label(self.mainFrame,text="To Open Account Click Below",font=("Times Roman",20,"bold"))
         opLabel.place(x=90,y=0)
         style = ttk.Style()
         style.theme_use("clam")
@@ -70,23 +78,23 @@ class bank():
                   background=[("active", "deepskyblue"),("pressed","white")])
         
         #add openAc button
-        openAcBtn=ttk.Button(mainFrame,command=self.openAc,text="Open Account",style="Blue.TButton")
+        openAcBtn=ttk.Button(self.mainFrame,command=self.openAc,text="Open Account",style="Blue.TButton")
         
         openAcBtn.place(x=80,y=40,width=280,height=60)
         #add deposit label
-        dpLabel=tk.Label(mainFrame,text="To Deposit",font=("Times Roman",20,"bold"))
+        dpLabel=tk.Label(self.mainFrame,text="To Deposit",font=("Times Roman",20,"bold"))
         dpLabel.place(x=160,y=130)
 
         #add deposit button
-        dpBtn=ttk.Button(mainFrame,command=self.deposit,text="Deposit",style="Blue.TButton")
+        dpBtn=ttk.Button(self.mainFrame,command=self.deposit,text="Deposit",style="Blue.TButton")
         dpBtn.place(x=80,y=170,width=280,height=60)
 
         #add withdraw label
-        wtLabel=tk.Label(mainFrame,text="To withdraw",font=("Times Roman",20,"bold"))
+        wtLabel=tk.Label(self.mainFrame,text="To withdraw",font=("Times Roman",20,"bold"))
         wtLabel.place(x=160,y=260)
 
         #add withdraw button
-        wtBtn=ttk.Button(mainFrame,text="Withdraw",style="Blue.TButton")
+        wtBtn=ttk.Button(self.mainFrame,text="Withdraw",style="Blue.TButton")
         wtBtn.place(x=80,y=300,width=280,height=60)
         #defining function of openAc button
     def openAc(self):
@@ -105,31 +113,32 @@ class bank():
         #adding to show user name input gui and taking entry
         usNameLabel=tk.Label(self.openAcFrame,text="Your Name :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         usNameLabel.grid(row=0,column=0,padx=20,pady=30)
-        self.usnameIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.usnameIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.usnameIn.grid(row=0,column=1,padx=20,pady=30)
+     
         
         #adding to show father name input gui and taking entry
         usfNameLabel=tk.Label(self.openAcFrame,text="Father Name :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         usfNameLabel.grid(row=1,column=0,padx=20,pady=30)
-        self.usfnameIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.usfnameIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.usfnameIn.grid(row=1,column=1,padx=20,pady=30)
         
         #adding to show mother name input gui and taking entry
         usmNameLabel=tk.Label(self.openAcFrame,text="Mother Name :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         usmNameLabel.grid(row=2,column=0,padx=20,pady=30)
-        self.usmnameIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.usmnameIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.usmnameIn.grid(row=2,column=1,padx=20,pady=30)
         
          #adding to show gender input gui and taking entry
         genLabel=tk.Label(self.openAcFrame,text="Gender :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         genLabel.grid(row=3,column=0,padx=20,pady=30)
-        self.genIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.genIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.genIn.grid(row=3,column=1,padx=20,pady=30)
         
          #adding to show nationality input gui and taking entry
         natLabel=tk.Label(self.openAcFrame,text="Nationality :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         natLabel.grid(row=4,column=0,padx=20,pady=30)
-        self.natIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.natIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.natIn.grid(row=4,column=1,padx=20,pady=30)
 
         #add label of contact detail
@@ -139,19 +148,19 @@ class bank():
          #adding to show phone number input gui and taking entry
         conLabel=tk.Label(self.openAcFrame,text="Phone Number :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         conLabel.grid(row=5,column=0,padx=20,pady=30)
-        self.conIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.conIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.conIn.grid(row=5,column=1,padx=20,pady=30)
         
          #adding to show email address input gui and taking entry
         emailLabel=tk.Label(self.openAcFrame,text="Email Id :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         emailLabel.grid(row=6,column=0,padx=20,pady=30)
-        self.emailIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.emailIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.emailIn.grid(row=6,column=1,padx=20,pady=30)
 
           #adding to show aadhar no input gui and taking entry
         aadharLabel=tk.Label(self.openAcFrame,text="Aadhar no. :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         aadharLabel.grid(row=7,column=0,padx=20,pady=30)
-        self.aadharIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.aadharIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.aadharIn.grid(row=7,column=1,padx=20,pady=30)
 
         #add label of address
@@ -169,25 +178,25 @@ class bank():
         #adding to show village input gui and taking entry
         villLabel=tk.Label(self.openAcFrame,text="Village :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         villLabel.grid(row=0,column=5,padx=20,pady=30)
-        self.villIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.villIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.villIn.grid(row=0,column=6,padx=20,pady=30)
         
         #adding to show district input gui and taking entry
         distLabel=tk.Label(self.openAcFrame,text="District :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         distLabel.grid(row=1,column=5,padx=20,pady=30)
-        self.distIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.distIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.distIn.grid(row=1,column=6,padx=20,pady=30)
 
         #adding to show state input gui and taking entry
         stateLabel=tk.Label(self.openAcFrame,text="State :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         stateLabel.grid(row=2,column=5,padx=20,pady=30)
-        self.stateIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.stateIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.stateIn.grid(row=2,column=6,padx=20,pady=30)
 
         #adding to show pin code input gui and taking entry
         pincLabel=tk.Label(self.openAcFrame,text="Pin Code :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         pincLabel.grid(row=3,column=5,padx=20,pady=30)
-        self.pincIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.pincIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.pincIn.grid(row=3,column=6,padx=20,pady=30)
 
          #add label of Nominee
@@ -197,13 +206,13 @@ class bank():
          #adding to nominee name input gui and taking entry
         nomLabel=tk.Label(self.openAcFrame,text="Nominee Name :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         nomLabel.grid(row=4,column=5,padx=20,pady=30)
-        self.nomIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.nomIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.nomIn.grid(row=4,column=6,padx=20,pady=30)
 
          #adding to relationship with nominee input gui and taking entry
         relLabel=tk.Label(self.openAcFrame,text="Relationship :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         relLabel.grid(row=5,column=5,padx=20,pady=30)
-        self.relIn=tk.Entry(self.openAcFrame,fg="white",width=15,font=("Times Roman",15))
+        self.relIn=tk.Entry(self.openAcFrame,width=15,font=("Times Roman",15))
         self.relIn.grid(row=5,column=6,padx=20,pady=30)
 
         submitBtn=ttk.Button(self.openAcFrame,text="Submit",command=self.final_submit,style="Blue.TButton")
@@ -296,38 +305,50 @@ class bank():
 
         #add label of money being deposited
         depoLabel=tk.Label(self.depositFrame,text="Amount to be deposited",font=("Times Roman",20,"bold"))
-        depoLabel.place(x=90,y=10)
+        depoLabel.place(x=120,y=10)
 
         self.depositFrame.grid_columnconfigure(0, minsize=60)
-        self.depositFrame.grid_rowconfigure(0, minsize=60)
+        self.depositFrame.grid_rowconfigure(0, minsize=40)
 
-        #adding to show money being deposited
+        #adding to show user name
         usenameLabel=tk.Label(self.depositFrame,text="User Name :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         usenameLabel.grid(row=1,column=1,padx=20,pady=30)
-        self.usenameIn=tk.Entry(self.depositFrame,fg="white",width=15,font=("Times Roman",15))
+        self.usenameIn=tk.Entry(self.depositFrame,width=15,font=("Times Roman",15))
         self.usenameIn.grid(row=1,column=2,padx=20,pady=30)
 
-        #adding to show money being deposited
+        #adding to show account_number
         acLabel=tk.Label(self.depositFrame,text="Account Number :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         acLabel.grid(row=2,column=1,padx=20,pady=30)
-        self.acIn=tk.Entry(self.depositFrame,fg="white",width=15,font=("Times Roman",15))
+        self.acIn=tk.Entry(self.depositFrame,width=15,font=("Times Roman",15))
         self.acIn.grid(row=2,column=2,padx=20,pady=30)
        
         
         #adding to show money being deposited
         depoLabel=tk.Label(self.depositFrame,text="Amount :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         depoLabel.grid(row=3,column=1,padx=20,pady=30)
-        self.depoIn=tk.Entry(self.depositFrame,fg="white",width=15,font=("Times Roman",15))
+        self.depoIn=tk.Entry(self.depositFrame,width=15,font=("Times Roman",15))
         self.depoIn.grid(row=3,column=2,padx=20,pady=30)
 
          #adding to show confirm money being deposited
         condepoLabel=tk.Label(self.depositFrame,text="Confirm Amount :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
         condepoLabel.grid(row=4,column=1,padx=20,pady=30)
-        self.condepoIn=tk.Entry(self.depositFrame,fg="white",width=15,font=("Times Roman",15))
+        self.condepoIn=tk.Entry(self.depositFrame,width=15,font=("Times Roman",15))
         self.condepoIn.grid(row=4,column=2,padx=20,pady=30)
 
+        #adding for description
+        desLabel=tk.Label(self.depositFrame,text="Description :",bg="light gray",fg="black",font=("Times Roman",15,"bold"))
+        desLabel.grid(row=5,column=1,padx=20,pady=30)
+        self.payment_type = ttk.Combobox(self.depositFrame, width=10,values=["Cash", "Online", "UPI", "Cheque"])
+        self.payment_type.set("Cash")
+        self.payment_type.grid(row=5,column=2,padx=20,pady=10)
+
+        #adding for submit button
         submitBtn=ttk.Button(self.depositFrame,text="Submit",command=self.finalsubmit,style="Blue.TButton")
-        submitBtn.grid(row=5,column=2,padx=20,pady=10)
+        submitBtn.grid(row=6,column=2,padx=20,pady=10)
+
+        #adding for back button
+        backBtn=ttk.Button(self.depositFrame,text="Back",command=self.create_main_frame,style="Blue.TButton")
+        backBtn.grid(row=6,column=1,padx=20,pady=10)
 
     def finalsubmit(self):
         
@@ -343,6 +364,7 @@ class bank():
         account_number = self.acIn.get()
         deposit_amt = self.depoIn.get()
         confirm_amt = self.condepoIn.get()
+        p_type= self.payment_type.get()
 
         # EMPTY CHECK
         if username == "" or account_number == "" or deposit_amt == "" or confirm_amt == "":
@@ -385,6 +407,9 @@ class bank():
             "UPDATE accounts SET balance = %s WHERE account_number = %s",
             (new_balance, account_number)
         )
+        self.cursor.execute(
+        "INSERT INTO deposit_history (account_no, amount, date_time, description) VALUES (%s, %s, NOW(), %s)",
+        (account_number, deposit_amt, self.payment_type.get()))
         self.db.commit()
 
         messagebox.showinfo("Success",
